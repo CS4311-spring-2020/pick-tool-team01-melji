644e674685ba692b6fcaf8fbe23d66d915a70a72
Eddie <eherrerag.eh@gmail.com>
2020-02-05 18:28:41 -0700

    Replaced the popups folder with the one that was send through Discord

--- src/ui/gui/popups/connect_link.py
+++ src/ui/gui/popups/connect_link.py
@@ -1,9 +1,3 @@
-"""
-Jazmin I Paz
-Feb 2 2020
-"""
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
@@ -17,20 +11,8 @@ class Ui_Dialog(object):
         self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.gridLayoutWidget)
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.gridLayout.addWidget(self.lineEdit_2, 1, 2, 1, 1)
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.gridLayoutWidget)
-        self.plainTextEdit.setObjectName("plainTextEdit")
-        self.gridLayout.addWidget(self.plainTextEdit, 2, 0, 1, 1)
-        self.lineEdit = QtWidgets.QLineEdit(self.gridLayoutWidget)
-        self.lineEdit.setObjectName("lineEdit")
-        self.gridLayout.addWidget(self.lineEdit, 1, 0, 1, 1)
-        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(self.gridLayoutWidget)
-        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
-        self.gridLayout.addWidget(self.plainTextEdit_2, 2, 2, 1, 1)
         self.horizontalLayoutWidget = QtWidgets.QWidget(Dialog)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(380, 150, 101, 31))
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(350, 150, 101, 31))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
@@ -39,7 +21,7 @@ class Ui_Dialog(object):
         self.pushButton.setObjectName("pushButton")
         self.horizontalLayout.addWidget(self.pushButton)
         self.verticalLayoutWidget = QtWidgets.QWidget(Dialog)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 101, 21))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(190, 70, 90, 30))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
@@ -57,16 +39,12 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Change Vector"))
-        self.lineEdit_2.setText(_translate("Dialog", "End date/time:"))
-        self.plainTextEdit.setPlainText(_translate("Dialog", "Description for selected vector:"))
-        self.lineEdit.setText(_translate("Dialog", "Start date/time:"))
-        self.plainTextEdit_2.setPlainText(_translate("Dialog", "Note for selected vector:"))
-        self.pushButton.setText(_translate("Dialog", "Change"))
+        Dialog.setWindowTitle(_translate("Dialog", "Connect link"))
+        self.pushButton.setText(_translate("Dialog", "Add"))
         self.comboBox.setItemText(0, _translate("Dialog", "None"))
-        self.comboBox.setItemText(1, _translate("Dialog", "Vector 1"))
-        self.comboBox.setItemText(2, _translate("Dialog", "Vector 2"))
-        self.comboBox.setItemText(3, _translate("Dialog", "Vector 3"))
+        self.comboBox.setItemText(1, _translate("Dialog", "Node 1"))
+        self.comboBox.setItemText(2, _translate("Dialog", "Node 2"))
+        self.comboBox.setItemText(3, _translate("Dialog", "Node 3"))
 
 
 if __name__ == "__main__":
