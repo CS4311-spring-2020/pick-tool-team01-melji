53dc5cf463e6cdf02b46cc24b8d69ff0a7585488
Eddie <eherrerag.eh@gmail.com>
2020-02-05 21:31:14 -0700

    Added the files that Leslie added in Discord

---
+++ src/ui/gui/popups/export_configuration.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'export_configuration.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(280, 117)
+        self.comboBox = QtWidgets.QComboBox(Form)
+        self.comboBox.setGeometry(QtCore.QRect(70, 30, 131, 22))
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(70, 10, 35, 10))
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(100, 70, 56, 17))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.comboBox.setItemText(0, _translate("Form", "None"))
+        self.comboBox.setItemText(1, _translate("Form", "PNG"))
+        self.comboBox.setItemText(2, _translate("Form", "JPEG"))
+        self.comboBox.setItemText(3, _translate("Form", "TXT"))
+        self.label.setText(_translate("Form", "Export as:"))
+        self.pushButton.setText(_translate("Form", "Export"))
